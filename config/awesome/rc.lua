f0c4942804b3924335ff752762e244cd27a12c73
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-02-11 23:47:39 +0200

    Add rofi key binding

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -319,7 +319,9 @@ globalkeys = gears.table.join(
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
     awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+              {description = "show the menubar", group = "launcher"}),
+    awful.key({ modkey }, "d", function () awful.spawn.with_shell("rofi -show drun") end,
+              {description = "rofi drun mode", group = "awesome"})
 )
 
 clientkeys = gears.table.join(
