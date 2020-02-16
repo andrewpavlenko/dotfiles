03982cdbd89852b94e18a513ae9136c815e1d7e1
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-02-16 14:05:44 +0200

    Remove layout images

--- config/awesome/themes/default/theme.lua
+++ config/awesome/themes/default/theme.lua
@@ -132,24 +132,6 @@ theme.titlebar_maximized_button_focus_active  = theme_path.."titlebar/maximized_
 
 theme.wallpaper = theme_path.."wallpaper.jpg"
 
--- You can use your own layout icons like this:
-theme.layout_fairh = theme_path.."layouts/fairhw.png"
-theme.layout_fairv = theme_path.."layouts/fairvw.png"
-theme.layout_floating  = theme_path.."layouts/floatingw.png"
-theme.layout_magnifier = theme_path.."layouts/magnifierw.png"
-theme.layout_max = theme_path.."layouts/maxw.png"
-theme.layout_fullscreen = theme_path.."layouts/fullscreenw.png"
-theme.layout_tilebottom = theme_path.."layouts/tilebottomw.png"
-theme.layout_tileleft   = theme_path.."layouts/tileleftw.png"
-theme.layout_tile = theme_path.."layouts/tilew.png"
-theme.layout_tiletop = theme_path.."layouts/tiletopw.png"
-theme.layout_spiral  = theme_path.."layouts/spiralw.png"
-theme.layout_dwindle = theme_path.."layouts/dwindlew.png"
-theme.layout_cornernw = theme_path.."layouts/cornernww.png"
-theme.layout_cornerne = theme_path.."layouts/cornernew.png"
-theme.layout_cornersw = theme_path.."layouts/cornersww.png"
-theme.layout_cornerse = theme_path.."layouts/cornersew.png"
-
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
     theme.menu_height, theme.bg_focus, theme.fg_focus
