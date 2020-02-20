97a6bbec7445574e3cdf256f01149893fa035317
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-02-20 12:44:27 +0200

    Back default layoutbox

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -127,7 +127,6 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 mytextclock = wibox.widget.textclock()
 
 -- Import some sweet widgets
-local layoutbox = require("sweet.layoutbox")
 local mybattery = require("sweet.battery")
 local myram = require("sweet.ram")
 local myvolume = require("sweet.volume")
@@ -179,6 +178,15 @@ awful.screen.connect_for_each_screen(function(s)
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
 
+    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(gears.table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
         screen  = s,
@@ -206,7 +214,7 @@ awful.screen.connect_for_each_screen(function(s)
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            layoutbox(s),
+            s.mylayoutbox,
         },
     }
 end)
