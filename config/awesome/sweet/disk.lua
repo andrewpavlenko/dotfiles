1f6afb3c8ad835ff1da422a1e84cbe0f1f79242b
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-03-02 21:10:03 +0200

    Change units to GB

--- config/awesome/sweet/disk.lua
+++ config/awesome/sweet/disk.lua
@@ -5,9 +5,9 @@ local beautiful = require("beautiful")
 local textbox = wibox.widget.textbox("")
 textbox.font = beautiful.nerd_font
 
-local disk = awful.widget.watch("df /dev/sda1 --output=pcent", 60, function(widget, stdout)
-  local out = stdout:match("(%d+)%%")
-  local text = " "..out.." %"
+local disk = awful.widget.watch("df /dev/sda1 -BG --output=avail", 60, function(widget, stdout)
+  local out = stdout:match("(%d+%a+)")
+  local text = " "..out.."B"
   widget.text = text
 end, textbox)
 
