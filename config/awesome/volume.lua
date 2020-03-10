87109181a5ba9a4e923ff50d89c9ae70203d77b8
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-03-10 12:29:07 +0200

    Added volume bar

--- config/awesome/volume.lua
+++ config/awesome/volume.lua
@@ -4,15 +4,11 @@ local beautiful = require("beautiful")
 local helpers = require("helpers")
 local gears = require("gears")
 
-local color = beautiful.volume_fg or "#56b6c2"
-local color_mute = beautiful.volume_mute_fg or "#5c6370"
 local step = 5
 
-local textbox = wibox.widget.textbox("")
-textbox.font = beautiful.nerd_font
-textbox.markup = helpers.colorize_text(" 0", color)
+local volume = {}
 
-local function get_volume_state(cb)
+volume.get_volume_state = function(cb)
   awful.spawn.easy_async("pactl list sinks", function(stdout)
     local mute = stdout:match("Mute:%s+(%a+)")
     local volume = stdout:match("%s%sVolume:[%s%a-:%d/]+%s(%d+)%%")
@@ -29,17 +25,8 @@ local function get_volume_state(cb)
   end)
 end
 
-local function update_widget(vol, mute)
-  local text = " "..vol.." %"
-  if mute then
-    textbox.markup = helpers.colorize_text(text, color_mute)
-  else
-    textbox.markup = helpers.colorize_text(text, color)
-  end
-end
-
-local function volume_up()
-  get_volume_state(function(vol, mute)
+volume.volume_up = function(cb)
+  volume.get_volume_state(function(vol, mute)
     local next_vol = vol + step
 
     if (next_vol > 100) then
@@ -47,12 +34,12 @@ local function volume_up()
     end
 
     os.execute("pactl set-sink-volume @DEFAULT_SINK@ "..next_vol.."%")
-    update_widget(next_vol, mute)
+    cb(next_vol, mute)
   end)
 end
 
-local function volume_down()
-  get_volume_state(function(vol, mute)
+volume.volume_down = function(cb)
+  volume.get_volume_state(function(vol, mute)
     local next_vol = vol - step
 
     if (next_vol < 0) then
@@ -60,26 +47,17 @@ local function volume_down()
     end
 
     os.execute("pactl set-sink-volume @DEFAULT_SINK@ "..next_vol.."%")
-    update_widget(next_vol, mute)
+    cb(next_vol, mute)
   end)
 end
 
-local function volume_mute()
-  get_volume_state(function(vol, mute)
+volume.volume_mute = function(cb)
+  volume.get_volume_state(function(vol, mute)
     local next_mute = mute and 0 or 1
     local next_mute_bool = not mute
     os.execute("pactl set-sink-mute @DEFAULT_SINK@ "..next_mute)
-    update_widget(vol, next_mute_bool)
+    cb(vol, next_mute_bool)
   end)
 end
 
-get_volume_state(update_widget)
-
-awesome.connect_signal("acpi::headphones", function() get_volume_state(update_widget) end)
-
-textbox:buttons(gears.table.join(
-    awful.button({ }, 4, volume_up),
-    awful.button({ }, 5, volume_down),
-    awful.button({ }, 1, volume_mute)))
-
-return textbox
+return volume
--- config/awesome/wibar.lua
+++ config/awesome/wibar.lua
@@ -2,9 +2,52 @@ local awful = require("awful")
 local gears = require("gears")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
 
 local helpers = require("helpers")
+local volume = require("volume")
 local text_taglist = require("taglist")
+
+-- Volume bar
+local volume_bar = wibox.widget {
+    max_value     = 100,
+    value         = 50,
+    forced_height = dpi(10),
+    forced_width  = dpi(70),
+    margins       = {
+      top = dpi(10),
+      bottom = dpi(10),
+    },
+    shape         = gears.shape.rounded_bar,
+    border_width  = 0,
+    color         = beautiful.xcolor6,
+    background_color = beautiful.xcolor0,
+    border_color  = beautiful.border_color,
+    widget        = wibox.widget.progressbar,
+}
+
+local function update_volume_bar(vol, mute)
+    volume_bar.value = vol
+    if mute then
+        volume_bar.color = beautiful.xforeground
+    else
+        volume_bar.color = beautiful.xcolor6
+    end
+end
+
+volume_bar:buttons(gears.table.join(
+    awful.button({ }, 4, function()
+        volume.volume_up(update_volume_bar)
+    end),
+    awful.button({ }, 5, function()
+      volume.volume_down(update_volume_bar)
+    end),
+    awful.button({ }, 1, function()
+      volume.volume_mute(update_volume_bar)
+    end)))
+
+volume.get_volume_state(update_volume_bar)
 
 -- Create systray widget
 local mysystray = wibox.widget.systray()
@@ -53,6 +96,7 @@ awful.screen.connect_for_each_screen(function(s)
         text_taglist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            volume_bar,
             mysystray_toggle,
         },
     }
