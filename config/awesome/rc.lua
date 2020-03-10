0eec588df882d6059bf6ec88aa9c8bb90192f79b
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-03-10 11:50:11 +0200

    Refactoring

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -128,6 +128,24 @@ require("acpid")
 -- Wibar
 require("wibar")
 
+local function set_wallpaper(s)
+    -- Wallpaper
+    if beautiful.wallpaper then
+        local wallpaper = beautiful.wallpaper
+        -- If wallpaper is a function, call it with the screen
+        if type(wallpaper) == "function" then
+            wallpaper = wallpaper(s)
+        end
+        gears.wallpaper.maximized(wallpaper, s, false)
+    end
+end
+
+-- Set wallpaper
+awful.screen.connect_for_each_screen(set_wallpaper)
+
+-- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
+screen.connect_signal("property::geometry", set_wallpaper)
+
 -- {{{ Mouse bindings
 root.buttons(gears.table.join(
     awful.button({ }, 3, function () mymainmenu:toggle() end),
--- config/awesome/wibar.lua
+++ config/awesome/wibar.lua
@@ -32,25 +32,7 @@ mysystray_toggle:buttons(gears.table.join(
     awful.button({}, 1, function() mysystray_popup.visible = not mysystray_popup.visible end)
 ))
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, false)
-    end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
