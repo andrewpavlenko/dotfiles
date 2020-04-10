23afd4d7a97126a592754f303328b69b857149dd
Andrei Pavlenko <pavlenkoandrei97@gmail.com>
2020-04-10 09:15:20 +0300

    Run polkit

--- config/awesome/autostart.sh
+++ config/awesome/autostart.sh
@@ -10,5 +10,8 @@ function run {
 run compton
 run redshift
 
+# Run GUI authentication manager. This command specific for package name on your distro
+run /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1
+
 # Keyboard layout
 setxkbmap -layout "us,ru,ua" -option "grp:alt_shift_toggle"
